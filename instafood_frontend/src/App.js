1aebeb5bb3da496d19754695ca7f2052cd5437d9
STeng618 <110608172+STeng618@users.noreply.github.com>
2023-05-17 14:59:32 +0800

    connect to firebase from react

--- instafood_frontend/src/App.js
+++ instafood_frontend/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { onSnapshot, collection } from 'firebase/firestore';
+import { db, storage, auth } from './firebase';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Instafood</p>
     </div>
   );
 }
