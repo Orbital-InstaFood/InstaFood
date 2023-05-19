b25e8c982abdcbae769e174f3d9e2a86fc69250b
STeng618 <110608172+STeng618@users.noreply.github.com>
2023-05-19 23:25:20 +0800

    created collection posts and set-up database to synchronise user-info and posts

---
+++ instafood_frontend/src/getUserDoc.js
@@ -0,0 +1,16 @@
+import { auth, db } from './firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
+async function getUserDoc() {
+    const user = auth.currentUser;
+
+    const userRef = doc(db, 'users', user.uid);
+    const snapshot = await getDoc(userRef);
+
+    if (snapshot.exists()) {
+        return snapshot;
+    }
+    return null;
+}
+
+export default getUserDoc;
--- instafood_frontend/src/pages/dashboard.js
+++ instafood_frontend/src/pages/dashboard.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import getUserDoc from '../getUserDoc';
 
-function Dashboard( {user} ) {
+function Dashboard() {
+    const [username, setUserName] = useState('');
+
+    useEffect(() => {
+        async function getUserInfo() {
+            const userDoc = await getUserDoc();
+            if (userDoc) {
+                const data = userDoc.data();
+                setUserName(data.username);
+            }
+        }
+        getUserInfo();
+    },[]);
+
     return (
         <div>
             <p>Dashboard</p>
-            <p>Welcome, {user.email}</p>
+            <p>Welcome, {username}</p>
         </div>
     );
 }
