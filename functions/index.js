4439f60161cd8d131643ca166892f4a6a0643b61
STeng618 <110608172+STeng618@users.noreply.github.com>
2023-06-01 00:33:51 +0800

    Cloud functions for deleting and creating comments

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,8 @@ const makeFollowRequest = require("./makeFollowRequest");
 const answerFollowRequest = require("./answerFollowRequest");
 const unfollow = require("./unfollow");
 const removeFollower = require("./removeFollower");
+const makeComment = require("./makeComment");
+const deleteComment = require("./deleteComment");
 
 exports.addUserID = addUserID.addUserID;
 exports.getListOfUserIDs = getListOfUserIDs.getListOfUserIDs;
@@ -14,3 +16,5 @@ exports.makeFollowRequest = makeFollowRequest.makeFollowRequest;
 exports.answerFollowRequest = answerFollowRequest.answerFollowRequest;
 exports.unfollow = unfollow.unfollow;
 exports.removeFollower = removeFollower.removeFollower;
+exports.makeComment = makeComment.makeComment;
+exports.deleteComment = deleteComment.deleteComment;
