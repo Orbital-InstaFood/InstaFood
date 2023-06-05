2f2fba2de8ef6a5fbff7ffc666672bff674e1bcd
STeng618 <110608172+STeng618@users.noreply.github.com>
2023-06-06 01:10:00 +0800

    Users can see new posts created by their following

--- instafood_frontend/src/pages/dashboard.js
+++ instafood_frontend/src/pages/dashboard.js
@@ -1,25 +1,41 @@
 import { useEffect, useState } from 'react';
 import getUserDoc from '../functions/getUserDoc';
-
+import DisplayArray from '../functions/DisplayArray';
+import DisplayPost from '../functions/DisplayPost'
 
 function Dashboard() {
-    const [username, setUserName] = useState('');
+    const [userProfile, setUserProfile] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function getUserInfo() {
             const userDoc = await getUserDoc();
             if (userDoc) {
-                const data = userDoc.data();
-                setUserName(data.username);
+                setUserProfile(userDoc.data());
+                setLoading(false);
             }
         }
         getUserInfo();
-    },[]);
+    }, []);
+
+    if (loading) {
+        return (
+            <div>
+                <p>Loading...</p>
+            </div>
+        );
+    }
 
     return (
         <div>
             <p>Dashboard</p>
-            <p>Welcome, {username}</p>
+            <p>Welcome, {userProfile.username}!</p>
+
+            <DisplayArray array={userProfile.postsToView} displayObjectFunc={postID =>
+                <DisplayPost
+                    postID={postID}
+                    userOwnID={userProfile.userID} />}
+            />
         </div>
     );
 }
