2ea3f08d152fee8327ad3285b1151d993ae82762
STeng618 <110608172+STeng618@users.noreply.github.com>
2023-06-10 16:49:08 +0800

    Take users back to app n sign them in after email verification

---
+++ instafood_frontend/src/authentication/signInAfterEmailVerification.js
@@ -0,0 +1,50 @@
+import { auth } from "../firebaseConf";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+import { useNavigate } from "react-router-dom";
+
+function SignInAfterEmailVerification () {
+    const navigate = useNavigate();
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const userEmail = urlParams.get("userEmail");
+    const password = urlParams.get("password");
+
+    // If user opens link on a different device, they will be prompted to sign in again
+    // If user opens link on the same device, they will be signed in automatically
+
+    signInWithEmailAndPassword(auth, userEmail, password)
+        .then((userCredential) => {
+            // Check that email address is verified
+            if (!userCredential.user.emailVerified ) {
+                alert("Please verify your email address before logging in.");
+                signOut(auth);
+            }
+            
+            navigate("/");
+        })
+        .catch((error) => {
+            if (error.code === "auth/invalid-email") {
+                alert("Invalid email address.");
+            }
+
+            if (error.code === "auth/user-not-found") {
+                alert("You do not have an account. Please create an account.");
+            }
+
+            if (error.code === "auth/wrong-password") {
+                alert("Incorrect password.");
+            }
+
+            // Check userEmail or password is null
+            if (userEmail === null || password === null) {
+                alert("Please sign in again.");
+            }
+
+            navigate("/");
+        });
+
+    return null;
+}
+
+export default SignInAfterEmailVerification;
