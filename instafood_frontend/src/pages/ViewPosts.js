406cfa86f9d7a4a9cd68cc8a360ef883f47c23ba
yyangdaa <zhang.yangda16@gmail.com>
2023-06-12 09:20:23 +0800

    enable searching by categories

--- instafood_frontend/src/pages/ViewPosts.js
+++ instafood_frontend/src/pages/ViewPosts.js
@@ -1,48 +1,59 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { collection, doc, getDocs, getDoc } from 'firebase/firestore';
+import { db } from '../firebaseConf';
+import {categoriesData} from '../theme/categoriesData.js';
 import categorySearch from '../functions/CategorySearch';
 import CaptionSearch from '../functions/CaptionSearch';
+import getPostsByPostIds from '../functions/postUtils';
 
 function ViewPosts() {
-  const [searchKeywordsCategories, setSearchKeywordsCategories] = useState('');
-  const [searchKeywordsCaptions, setSearchKeywordsCaptions] = useState('');
+  const [searchCategory, setSearchCategory] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
-  const filteredCategories = categorySearch(searchKeywordsCategories);
-  const filteredPosts = CaptionSearch(searchKeywordsCaptions);
+  const handleSearch = async () => {
+    // Retrieve the post IDs from the categorisedPosts collection
+    const categorisedPostsRef = doc(db, 'categorisedPosts', searchCategory);
+    const categorisedPostsDoc = await getDoc(categorisedPostsRef);
+    const postIds = categorisedPostsDoc.data()?.post_id_array || [];
+
+    const posts = await getPostsByPostIds(postIds);
+    setSearchResults(posts);
+
+    console.log(posts); // Array of posts with their information
+    
+  };
 
   return (
     <div>
       <h1>View Posts</h1>
       
-      
-      <input 
-        type="text"
-        placeholder="Search categories"
-        value={searchKeywordsCategories}
-        onChange={(e) => setSearchKeywordsCategories(e.target.value)}
-      />
-
-      <input 
-        type="text"
-        placeholder="Search captions"
-        value={searchKeywordsCaptions}
-        onChange={(e) => setSearchKeywordsCaptions(e.target.value)}
-      />
-
-      <div>
-        <h2>Categories</h2>
-        {filteredCategories.map((category, index) => (
-          <p key={index}>{category}</p>
+       <select
+          id="category"
+          value={searchCategory}
+          onChange={(e) => setSearchCategory(e.target.value)}
+        > 
+          <option value="">Search a category</option>
+          {categoriesData.map((category,index) => (
+            <option key={index} value={index}>
+              {category}
+            </option>
+          ))}
+        </select>
+        
+        <button onClick={handleSearch}>Search</button>
+        
+        {searchResults.map((post) => (
+        <div key={post.post_id}>
+          <h2>{post.title}</h2>
+          <p>{post.caption}</p>
+          {post.images.map((image) => (
+          <img key={image} src={image} alt="Post Image" />
+          ))}
+          <p>Likes: {post.likes.length}</p>
+          <p>Comments: {post.comments.length}</p>
+          </div>
         ))}
-      </div>
-
-      <div>
-        <h2>Posts</h2>
-        {filteredPosts.map((post, index) => (
-          <p key={index}>{post.caption}</p>
-        ))}
-      </div>
-    </div>
-  );
+        </div>);
 }
 
 export default ViewPosts;
