f7ddde4d8aa88dffe5cf294bf861bb774ae9504d
STeng618 <110608172+STeng618@users.noreply.github.com>
2023-06-12 14:39:38 +0800

    Conditional Navbar for access control, more routes

--- instafood_frontend/src/App.js
+++ instafood_frontend/src/App.js
@@ -1,24 +1,37 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import { auth } from './firebaseConf';
 
 import Login from './authentication/login';
-import Signup from './authentication/signup';
+import SignUp from './authentication/signup';
 import Logout from './authentication/logout';
 import SignInAfterEmailVerification from './authentication/signInAfterEmailVerification';
 import ForgotPassword from './authentication/forgotPassword';
 
 import Dashboard from './pages/Dashboard';
+import CreateProfile from './pages/CreateProfile';
 import UserInfo from './pages/UserInfo';
 import NewPost from './pages/NewPost';
 import Connect from './pages/Connect';
 import ViewOtherUsers from './pages/ViewOtherUsers';
 import Navbar from './Navbar';
 
+import PageNotFound from './pages/404';
+
 function App() {
   const [user, setUser] = useState(null);
+
+  function NavbarConditional() {
+    const location = useLocation();
+    const { pathname } = location;
+  
+    if (pathname === '/createProfile') {
+      return null; 
+    }
+    return <Navbar />;
+  }
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -26,33 +39,32 @@ function App() {
     });
   }, []);
 
-  if (user && user.emailVerified) {
-    return (
-      <Router>
-        <h1>InstaFood</h1>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/newPost" element={<NewPost />} />
-          <Route path="/editProfile" element={<UserInfo />} />
-          <Route path="/connect" element={<Connect />} />
-          <Route exact path="/:userID" element={<ViewOtherUsers />} />
-
-        </Routes>
-        <Logout />
-      </Router>
-    );
-  }
-
   return (
     <Router>
       <h1>InstaFood</h1>
+
+      {user && user.emailVerified && <NavbarConditional />}
+
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
         <Route path="/signInAfterEmailVerification" element={<SignInAfterEmailVerification />} />
+        {user && user.emailVerified ? (
+          <>
+            <Route path="/createProfile" element={<CreateProfile />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/newPost" element={<NewPost />} />
+            <Route path="/editProfile" element={<UserInfo />} />
+            <Route path="/connect" element={<Connect />} />
+            <Route path="/:userID" element={<ViewOtherUsers />} />
+          </>
+        )
+          : null}
+          <Route path="*" element={<PageNotFound />} />
       </Routes>
+
+      {user && user.emailVerified && <Logout /> }
     </Router>
   );
 }
