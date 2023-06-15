5bb6af33e94c49ad771685238620c57f431cc001
STeng618 <110608172+STeng618@users.noreply.github.com>
2023-06-15 17:17:26 +0800

    Listener classes with singleton+observer patterns

---
+++ instafood_frontend/src/listeners/ListenerClass.js
@@ -0,0 +1,62 @@
+import { onSnapshot, getDoc } from 'firebase/firestore';
+
+class Listener {
+    constructor(ref) {
+        this.ref = ref;
+        this.document = null;
+        this.loading = true;
+        this.fieldSubscriptions = {};
+        this.unsubscribeFromListener = null;
+    }
+
+    async startSnapshotListener() {
+        // Read the document once to initialize the property
+        const snapshot = await getDoc(this.ref);
+        this.document = snapshot.data();
+        this.loading = false;
+        this.notifySubscribers();
+
+        this.unsubscribeFromListener = onSnapshot(this.ref, (latestSnapshot) => {
+            this.document = latestSnapshot.data();
+            this.notifySubscribers();
+        });
+    }
+
+    stopSnapshotListener() {
+        this.unsubscribeFromListener();
+    }
+
+
+    subscribeToField(field, callback) {
+        if (!this.fieldSubscriptions[field]) {
+            this.fieldSubscriptions[field] = [];
+        }
+        this.fieldSubscriptions[field].push(callback);
+
+        // Call the callback immediately with the current value
+        const currentValue = this.document[field];
+        callback(currentValue);
+
+        return () => {
+            this.unsubscribeFromField(field, callback);
+        }
+
+    }
+
+    notifySubscribers() {
+        for (const field in this.fieldSubscriptions) {
+            const callbacksBySubscribers = this.fieldSubscriptions[field];
+            for (const callback of callbacksBySubscribers) {
+                callback(this.document[field]);
+            }
+        }
+    }
+
+    unsubscribeFromField(field, callback) {
+        const callbacksBySubscribers = this.fieldSubscriptions[field];
+        const index = callbacksBySubscribers.indexOf(callback);
+        this.fieldSubscriptions[field].splice(index, 1);
+    }
+}
+
+export default Listener;
---
+++ instafood_frontend/src/listeners/ListenerImplementer.js
@@ -0,0 +1,40 @@
+import Listener from "./ListenerClass";
+import { doc } from "firebase/firestore";
+import { db, auth } from "../firebaseConf";
+
+class ListenerImplementer {
+    constructor() {
+        this.userDocListener = null;
+        this.listOfUserIDsListener = null;
+    }
+
+    async getPostListener(postID) {
+        const postRef = doc (db, 'posts', postID);
+        const postListener = new Listener(postRef);
+        await postListener.startSnapshotListener();
+        return postListener;
+    }
+
+    async getUserDocListener() {
+        if (!this.userDocListener) {
+            const userRef = doc(db, 'users', auth.currentUser.uid);
+            const userDocListener = new Listener(userRef);
+            await userDocListener.startSnapshotListener();
+            this.userDocListener = userDocListener;
+        } 
+        return this.userDocListener;
+    }
+
+    async getListOfUserIDsListener() {
+        if (!this.listOfUserIDsListener) {
+            const userIDsRef = doc(db, "lists", "userIDs");
+            const listOfUserIDsListener = new Listener(userIDsRef);
+            await listOfUserIDsListener.startSnapshotListener();
+            this.listOfUserIDsListener = listOfUserIDsListener;
+        }
+        return this.listOfUserIDsListener;
+    }
+}
+
+const listenerImplementer = new ListenerImplementer();
+export default listenerImplementer;
