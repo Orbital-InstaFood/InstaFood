0a04b5468086cd04f6228e1f79c80fbed8a72692
Zhang Yangda <yangda@Brililiant-Genius-MacBook-Air.local>
2023-06-19 23:55:44 +0800

    rank post modified and moved to cloud function

--- functions/RankPost.js
+++ functions/RankPost.js
@@ -1,9 +1,42 @@
-const engagementWeight = 0.5;
-//const popularityWeight = 0.5;
+const functions = require('firebase-functions');
+const { getFirestore } = require('firebase-admin/firestore');
+const db = getFirestore();
 
-//function calculatePopularityScore(post) {
- // return post.userFollowers;
-//}
+const engagementWeight = 0.5;
+const popularityWeight = 0.5;
+
+exports.rankPosts = functions.https.onCall(async (data, context) => {
+  const rankedPosts = [...posts];
+  rankedPosts.forEach((post, index) => {
+    const { posts } = data;
+    const postRef = db.collection('posts').doc(postID);
+    const postDoc = await postRef.get();
+    const post = postDoc.data();
+    const userRef = db.collection('users').doc(post.creator);
+    const userDoc = await userRef.get();
+    const userFollowers = userDoc.data().followers;
+    const engagementScore = post.likes + post.comments + post.shares;
+    const popularityScore = userFollowers;
+    const rankScore =  engagementScore * engagementWeight + popularityScore * popularityWeight
+    rankedPosts[index].rankScore = rankScore;
+  };
+  rankedPosts.sort((a, b) => b.rankScore - a.rankScore);
+  return { rankedPosts };
+});
+
+/*
+  const sortedPosts = await Promise.all(posts.map(async (post) => {
+    const userRef = db.collection('users').doc(post.creator);
+    const userDoc = await userRef.get();
+    const userFollowers = userDoc.data().followers;
+  return { rankedPosts };
+  }));
+});
+
+function calculatePopularityScore(post) {
+ return post.userFollowers;
+}
+
 /*
 function calculateFreshnessScore(post) {  
   const postDate = new Date(post.timestamp);
@@ -12,7 +45,7 @@ function calculateFreshnessScore(post) {
   const monthsDifference = (1000 * 3600 * 24*30) / timeDifference;
   return monthsDifference;
 }*/
-
+/*
 function calculateEngagementScore(post){
   return post.likes + post.comments + post.shares;
 }
@@ -22,11 +55,10 @@ function rankPosts(posts) {
 
     rankedPosts.forEach((post, index) => {
       post.engagementScore = calculateEngagementScore(post); 
-//      post.popularityScore = calculatePopularityScore(post); 
+      post.popularityScore = calculatePopularityScore(post); 
   
       post.rankScore = (
-        post.engagementScore * engagementWeight +
-   //     post.popularityScore * popularityWeight +
+        post.engagementScore * engagementWeight + post.popularityScore * popularityWeight
       );
   
       rankedPosts[index] = post;
@@ -36,3 +68,4 @@ function rankPosts(posts) {
   
     return rankedPosts;
   }
+
