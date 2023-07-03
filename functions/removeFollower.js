91171eb532dec2a8cd62fb32ea3062511c71e7a9
STeng618 <110608172+STeng618@users.noreply.github.com>
2023-07-03 23:37:39 +0800

    Remove posts from toView when unfollowing/ removing followers

--- functions/removeFollower.js
+++ functions/removeFollower.js
@@ -21,11 +21,22 @@ exports.removeFollower = functions.https.onCall(async (request) => {
     const followedSnapshot = await db.collection('backend_userID_UID').doc(followedUserID).get();
     const followedUID = followedSnapshot.data().UID;
 
+    const followedUserDocSnapshot = await db.collection('users').doc(followedUID).get();
+    const postsToRemoveFromToView = followedUserDocSnapshot.data().personalPosts;
+
     await db.collection('users').doc(followerUID).update(
         {
             following: admin.firestore.FieldValue.arrayRemove(followedUserID),
         }
     );
+
+    for (const postID of postsToRemoveFromToView) {
+        await db.collection('users').doc(followerUID).update(
+            {
+                postsToView: admin.firestore.FieldValue.arrayRemove(postID),
+            }
+        );
+    }
 
     await db.collection('users').doc(followedUID).update(
         {
