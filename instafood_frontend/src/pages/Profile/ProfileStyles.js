2cb8008eab1938506445c638c34bc563419e1c40
STeng618 <110608172+STeng618@users.noreply.github.com>
2023-07-03 23:48:20 +0800

    UI for createProfile. Restrictions on characters allowed for user ids

--- instafood_frontend/src/pages/Profile/ProfileStyles.js
+++ instafood_frontend/src/pages/Profile/ProfileStyles.js
@@ -1,5 +1,8 @@
-import { styled } from '@mui/material/styles';
-import { Box } from '@mui/system';
+import {
+  Box,
+  Typography,
+  styled,
+} from "@mui/material";
 
 const UserInfoContainer = styled(Box)`
   flex-direction: column,
@@ -12,4 +15,33 @@ const UserInfoContainer = styled(Box)`
   padding: 1rem; 
   `;
 
-export { UserInfoContainer };
+  const Title = styled(Typography)`
+font-size: 1.5rem;
+font-weight: bold;
+`;
+
+const CheckboxButtonContainer = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  width: 100%;
+`;
+
+const Container = styled(Box)`
+  flex-direction: column;
+  margin-top: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 1rem;
+  width: "100%";
+  border: 1px solid #ccc; 
+  padding: 1rem; 
+  `;
+
+export { 
+  UserInfoContainer, 
+  Title, 
+  CheckboxButtonContainer, 
+  Container
+};
