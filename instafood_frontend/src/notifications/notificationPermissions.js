adc8858e3b9364d5ccb379758ee248fa2beaeabd
yyangdaa <zhang.yangda16@gmail.com>
2023-07-11 18:11:10 +0800

    firebase cloud messaging token / users will have to select the notification permission at the dashboard when they first log in

--- instafood_frontend/src/notifications/notificationPermissions.js
+++ instafood_frontend/src/notifications/notificationPermissions.js
@@ -1,26 +1,44 @@
+import { auth, db, functions } from '../firebaseConf';
+import { getFCMToken } from './fcmTokenService';
+
 function requestNotificationPermission() {
-    if ('Notification' in window) {
-      Notification.requestPermission().then(function (permission) {
-        if (permission === 'granted') {
-          console.log('Notification permission granted');
-        } else if (permission === 'denied') {
-          console.log('Notification permission denied');
-        } else if (permission === 'default') {
-          console.log('Notification permission dismissed');
+  if ('Notification' in window) {
+    Notification.requestPermission().then(async function (permission) {
+      if (permission === 'granted') {
+        console.log('Notification permission granted');
+
+        const user = auth.currentUser;
+        const uid = user.uid;
+
+        const token = await getFCMToken();
+
+        try {
+          const userDocRef = doc(db, 'users', uid);
+          await updateDoc(userDocRef, {
+            fcmToken: token,
+          });
+
+          console.log('FCM token stored in user document:', token);
+        } catch (error) {
+          console.log('Failed to store FCM token in user document:', error);
         }
-      });
-    } else {
-      console.log('Notifications not supported in this browser');
-    }
+      } else if (permission === 'denied') {
+        console.log('Notification permission denied');
+      } else if (permission === 'default') {
+        console.log('Notification permission dismissed');
+      }
+    });
+  } else {
+    console.log('Notifications not supported in this browser');
   }
-  
-  function showNotification(title, options) {
-    if ('Notification' in window && Notification.permission === 'granted') {
-      navigator.serviceWorker.ready.then(function (registration) {
-        registration.showNotification(title, options);
-      });
-    } else {
-      console.log('Notification permission not granted');
-    }
+}
+
+function showNotification(title, options) {
+  if ('Notification' in window && Notification.permission === 'granted') {
+    navigator.serviceWorker.ready.then(function (registration) {
+      registration.showNotification(title, options);
+    });
+  } else {
+    console.log('Notification permission not granted');
   }
-  
+}
